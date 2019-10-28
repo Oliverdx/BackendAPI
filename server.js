a61ebb668badd9a9e4f45a215506c0049b0f1bd7
Vanderlei <vanderlei327@gmail.com>
2019-10-28 17:52:50 -0300

    Fix Database connection

--- server.js
+++ server.js
@@ -10,14 +10,10 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 MongoClient.connect(db.url, { useUnifiedTopology: true }, (err, database) => {
-    if (err) return console.log(err);
-    const db = database.db('bikefix');
+    if (err) return console.log(err)
+    // Make sure you add the database name and not the collection name 
+    const dbase = database.db("bikefix")
 
-    require('./app/routes')(app, db);
-
-    database.close();
-});
-
-
-
-app.listen(port, () => { console.log('We are live on ' + port); });
+    require('./app/routes')(app, dbase);
+    app.listen(port, () => { console.log('We are live on ' + port); });
+})
