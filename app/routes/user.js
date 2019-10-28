ddbfa18194f81810b90b1ef5bd33a0cea11862af
Vanderlei <vanderlei327@gmail.com>
2019-10-28 17:53:00 -0300

    Basic user API routes

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,38 +1,88 @@
 module.exports = function (app, db) {
 
-    /* Lista usuários */
-
-    app.post('/users', (req, res) => {
-        res.send('Exibindo lista de usuários');
-    });
-
-    /* Consulta usuário no Banco */
-
-    app.get('/users/get-users', (req, res) => {
-        res.send('Buscando usuário no Banco...');
-    });
-
+    let usersCollection = db.collection('users');
 
     /* Cria Usuário */
 
     app.post('/users/new-user', (req, res) => {
-        console.log(req.body);
 
-        const user = { user: req.body.user, level: req.body.level };
-        db.collection('users').insert(user, (err, result) => {
-            if (err) {
-                res.send({ 'error': 'An error has ocurred' });
-            } else {
-                res.send(result.ops[0]);
-            }
+        const user = {
+            user: req.body.user.toLowerCase(),
+            name: req.body.name,
+            level: req.body.level.toLowerCase()
+        };
+
+        usersCollection.insert(user, (err, sucess) => {
+
+            if (err)
+                res.send(`Error when trying to insert user in DB ${err}`);
+
+            res.send('User inserted sucessfully');
         });
 
     });
 
+    /* Lista usuários */
+
+    app.get('/users/get-users', (req, res) => {
+        usersCollection.find({}).toArray((err, result) => {
+            if (err)
+                throw err;
+            res.json(result);
+        });
+    });
+
+
+    /* Busca usuário pelo username */
+
+    app.get('/users/find/:username', (req, res, next) => {
+        const username = req.params.username ? req.params.username : null;
+
+        if (!username)
+            return res.send('Invalid username');
+
+        usersCollection.find({ user: username }).toArray((err, result) => {
+            if (err)
+                throw err;
+            res.json(result);
+        });
+    });
+
+    /* Atualiza usuário */
+
+    app.put('/users/update/:username', (req, res) => {
+        const username = req.params.username ? req.params.username : null;
+        const data = req.body;
+
+        try {
+            usersCollection.updateOne({ user: username }, { $set: data }).then(data => {
+                if (data.modifiedCount > 0)
+                    return res.send('Usuário atualizado com sucesso');
+
+                res.send('Usuário não encontrado')
+            });
+        } catch (err) {
+            return res.send('Erro ao atualizar o usuário');
+        }
+    });
+
     /* Deleta usuário */
 
-    app.delete('/users/delete', (req, res) => {
-        res.send('Usuário Deletado');
+    app.delete('/users/delete/:username', (req, res) => {
+        const username = req.params.username ? req.params.username : null;
+
+        if (!username)
+            res.send('Invalid username');
+
+        usersCollection.removeOne({ user: username }, (err, result) => {
+            if (err)
+                return res.send(err);
+
+            if (result.deletedCount > 0)
+                return res.send(`User ${username} deleted sucessfully`);
+
+            res.send(`User not found`);
+        });
     });
 
 };
